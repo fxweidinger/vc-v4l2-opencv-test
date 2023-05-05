f692ad0801dae8c5aa413c6888ead4b91359f81f
fxweidinger <felix-weidinger@outlook.de>
2023-05-05 11:21:29 +0200

    removed cmake-build-debug-remote from tracking

--- cmake-build-debug-remote/CMakeFiles/vc_v4l2_opencv_test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for vc_v4l2_opencv_test.
-# This may be replaced when dependencies are built.
--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <cstdio>
 
+using namespace cv;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Mat frame;
+    VideoCapture cap;
+
+    cap.open(0);
+
+    if(!cap.isOpened()){
+        std::cerr<<"Error! Can't open camera";
+        return -1;
+    }
+    //--- GRAB AND WRITE LOOP
+    std::cout << "Start grabbing" << std::endl
+         << "Press any key to terminate" << std::endl;
+    for (;;)
+    {
+        // wait for a new frame from camera and store it into 'frame'
+        cap.read(frame);
+        // check if we succeeded
+        if (frame.empty()) {
+            std::cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
+        // show live and wait for a key with timeout long enough to show images
+        std::cout<<"Frame wasn't empty"<< std::endl;
+    }
+    // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
