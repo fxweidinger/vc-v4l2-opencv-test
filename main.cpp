668213ccb86e0b7761c4ef802001626d62eb1096
fxweidinger <felix-weidinger@outlook.de>
2023-05-09 10:29:28 +0200

    directory for image acquisition tests added

--- main.cpp
+++ main.cpp
@@ -1,34 +1,48 @@
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include <cstdio>
 
 using namespace cv;
 int main() {
     Mat frame;
-    VideoCapture cap;
-
+    VideoCapture cap(CAP_V4L2);
     cap.open(0);
 
     if(!cap.isOpened()){
         std::cerr<<"Error! Can't open camera";
         return -1;
     }
-    //--- GRAB AND WRITE LOOP
+
+    std::cout<<"Convert Flag : "<<cap.get(CAP_PROP_CONVERT_RGB)<<std::endl;
+    cap.set(CAP_PROP_CONVERT_RGB ,0);
+    std::cout<<"Convert Flag Toggle? : "<<cap.get(CAP_PROP_CONVERT_RGB)<<std::endl;
+
+    std::cout<<"FPS: "<<cap.get(CAP_PROP_FPS)<<std::endl;
+    cap.set(CAP_PROP_FPS ,60.0);
+    std::cout<<"FPS Set? : "<<cap.get(CAP_PROP_FPS)<<std::endl;
+
+
+
+
     std::cout << "Start grabbing" << std::endl
          << "Press any key to terminate" << std::endl;
-    for (;;)
-    {
+    //for (;;)
+    //{
         // wait for a new frame from camera and store it into 'frame'
         cap.read(frame);
         // check if we succeeded
         if (frame.empty()) {
             std::cerr << "ERROR! blank frame grabbed\n";
-            break;
+            return -1;
         }
         // show live and wait for a key with timeout long enough to show images
-        std::cout<<"Frame wasn't empty"<< std::endl;
-    }
+        //std::ctime(reinterpret_cast<const time_t *>(std::chrono::system_clock::to_time_t(
+        //                std::chrono::system_clock::now())))
+        std::cout<<"Frame info: "<<frame.type()<<std::endl;
+        imwrite("/home/fue/image-acq-tests/test.jpg",frame);
+    //}
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
